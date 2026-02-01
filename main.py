bcb5af2ae6445b9d63ed1aedf6e408e327155255
N0tstgr <vikashkasaudhan321@gmail.com>
2026-02-01 17:44:48 +0530

    json-file reading

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 # read data from the csv files
 
-fd = pd.read_csv("sales_data_sample.csv" ,encoding="latin1")
-print(fd)
+# fd = pd.read_csv("sales_data_sample.csv" ,encoding="latin1")
+# fd = pd.read_excel("FSI-2023-DOWNLOAD.xlsx"
+df = pd.read_json("titanic.json")
+
+
+
+print(df)
