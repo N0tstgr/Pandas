3d53da33e06d76bf43abfd4f35875198c16e5a54
N0tstgr <vikashkasaudhan321@gmail.com>
2026-02-04 06:33:34 +0530

    describe

---
+++ Describe.py
@@ -0,0 +1,13 @@
+#sample data frame using dictionary
+import pandas as pd
+
+info = {
+    "city": ['New York', 'Hong-Kong', 'shezeng', 'California', 'Las-vegas','Mumbai', 'Washington', 'manhattan'],
+    "companiestenure": [10,45,90,34,32, 55,39,53],
+    "revenueInBillion": [3, 500,2000,450,323,49,200,400],
+    "Performance": [96,96,93,92,91,90,98,99]
+
+}
+bb = pd.DataFrame(info)
+print("Demo DataFrame")
+print(bb)
