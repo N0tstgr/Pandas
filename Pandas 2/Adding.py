00ee76f94366c67d985c51d90f8b83fb6530e7fb
N0tstgr <vikashkasaudhan321@gmail.com>
2026-02-06 06:32:53 +0530

    adding-Panda2

---
+++ Pandas 2/Adding.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+info = {
+    "city": ['New York', 'Hong-Kong', 'shenzeng', 'California', 'Las-vegas','Mumbai', 'Washington', 'manhattan'],
+    "companiestenure": [10,45,90,34,32, 55,39,53],
+    "revenueInBillion": [3, 500,2000,450,323,49,200,400],
+    "Performance": [96,96,93,92,91,90,98,99]
+
+}
+
+gg = pd.DataFrame(info)
+print(gg)
+gg["IncrementInRevenue"] = gg['revenueInBillion']*0.3
+print(gg)
