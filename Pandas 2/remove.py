6efb868792a7b0be14c59ca45c422f51e82e68be
N0tstgr <vikashkasaudhan321@gmail.com>
2026-02-08 06:58:24 +0530

    remove

--- Pandas 2/remove.py
+++ Pandas 2/remove.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-info1 = {
+info2 = {
     "city": ['New York', 'Hong-Kong', 'shenzeng', 'California', 'Las-vegas','Mumbai', 'Washington', 'manhattan'],
     "companiestenure": [10,45,90,34,32, 55,39,53],
     "revenueInBillion": [3, 500,2000,450,323,49,200,400],
@@ -9,8 +9,9 @@ info1 = {
 
 }
 
-gg = pd.DataFrame(info1)
+gg = pd.DataFrame(info2)
 print(gg)
-#increasing the salary by 1.2%
-gg["Performance"] = gg["Performance"] * 1.2
+
+#gg.drop(columns = ["ColumnName"], inplaceTrue)
+gg.drop(columns = ["Performance"], inplace=True)
 print(gg)
