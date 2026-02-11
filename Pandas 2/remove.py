9a0f0a00f1f78cd82dfa19bc009e238def1c657d
N0tstgr <vikashkasaudhan321@gmail.com>
2026-02-11 06:40:04 +0530

    per

--- Pandas 2/remove.py
+++ Pandas 2/remove.py
@@ -5,7 +5,7 @@ info2 = {
     "city": ['New York', 'Hong-Kong', 'shenzeng', 'California', 'Las-vegas','Mumbai', 'Washington', 'manhattan'],
     "companiestenure": [10,45,90,34,32, 55,39,53],
     "revenueInBillion": [3, 500,2000,450,323,49,200,400],
-    "Performance": [96,96,93,92,91,90,98,99]
+    "Performance": [96,96,93,92,91,90,98,98]
 
 }
 
